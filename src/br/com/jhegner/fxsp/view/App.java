4fc37e04e4e4df1d9854778d062e7fe78088de2e
Jidlafe Hegner <jidlafehegner@gmail.com>
2018-12-12 00:12:38 -0200

    Inicio do projeto, adicionado a classe principal App

---
+++ src/br/com/jhegner/fxsp/view/App.java
@@ -0,0 +1,24 @@
+package br.com.jhegner.fxsp.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Classe principal do app.
+ * 
+ * @author Jidlafe Hegner
+ * @since 11/12/2018
+ *
+ */
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
