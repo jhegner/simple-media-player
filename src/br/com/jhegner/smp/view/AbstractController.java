16d60bdb08fb200f5e3480db18a0b35f6552f961
Jidlafe Hegner <jidlafehegner@gmail.com>
2020-05-17 01:09:05 -0300

    Abstract Controller para acoes comuns

---
+++ src/br/com/jhegner/smp/view/AbstractController.java
@@ -0,0 +1,36 @@
+package br.com.jhegner.smp.view;
+
+import br.com.jhegner.smp.enums.EBotao;
+import br.com.jhegner.smp.enums.EMidia;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ButtonBase;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * Abstract Controller fxml
+ * 
+ * @author jhegner
+ *
+ */
+public abstract class AbstractController {
+	
+	void fecharJanela(ActionEvent currentEvent) {
+		Node source = (Node) currentEvent.getSource();
+		Stage current = (Stage) source.getScene().getWindow();
+		current.close();
+	}
+	
+	void configuraBotoes(Scene scene, EMidia midia) {
+		
+		for(EBotao botao : EBotao.values()) {
+			ButtonBase btnConf = (ButtonBase) scene.lookup(botao.getId());
+			btnConf.setGraphic(new ImageView(new Image(getClass().getResourceAsStream(botao.getImgPath()))));
+			btnConf.setTooltip(new Tooltip(botao.getTooltip()));
+		}
+	}
+}
