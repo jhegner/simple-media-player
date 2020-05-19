5777eb834e0fb3158b2774e5a3bb2aa4f42a97e8
Jidlafe Hegner <jidlafehegner@gmail.com>
2020-05-19 16:43:06 -0300

    Adicionado representacao da classe Arquivo

---
+++ src/br/com/jhegner/smp/media/Arquivo.java
@@ -0,0 +1,39 @@
+package br.com.jhegner.smp.media;
+
+import java.io.Serializable;
+
+import org.apache.commons.io.FilenameUtils;
+
+import br.com.jhegner.smp.enums.EMidia;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Representacao de um arquivo de media. 
+ * 
+ * @author jhegner
+ *
+ */
+@Getter
+@AllArgsConstructor
+public class Arquivo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private final String nome;
+	private final String local;
+	private final long tamanho;
+	private final EMidia midia;
+	
+	public String getNomeBase() {
+		return FilenameUtils.getBaseName(getNome());
+	}
+	
+	public String getNomeCompleto() {
+		return (getNome() + getLocal());
+	}
+	
+	public String getExtensao() {
+		return FilenameUtils.getExtension(getNome());
+	}
+}
