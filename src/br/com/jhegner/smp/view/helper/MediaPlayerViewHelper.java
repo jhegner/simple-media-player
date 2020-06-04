3e32cd1901b206a509ca9e0569a1cfffea9bb043
Jidlafe Hegner <jidlafehegner@gmail.com>
2020-06-04 00:55:42 -0300

    Apresentacao da imagem do album

--- src/br/com/jhegner/smp/view/helper/MediaPlayerViewHelper.java
+++ src/br/com/jhegner/smp/view/helper/MediaPlayerViewHelper.java
@@ -1,10 +1,18 @@
 package br.com.jhegner.smp.view.helper;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import br.com.jhegner.smp.domain.Arquivo;
 import br.com.jhegner.smp.domain.ArquivoAudio;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MediaPlayerViewHelper {
 	
 	public void exibeMetadadosMedia(Arquivo arquivo, Scene scene) {
@@ -21,9 +29,50 @@ public class MediaPlayerViewHelper {
 	
 			final Label labelDuracao = (Label) scene.lookup("#labelDuracao");
 			labelDuracao.setText(arquivoAudio.getDuracao());
-		
+			
+			final ImageView ivAudio = (ImageView) scene.lookup("#imageViewAlbumImage");
+
+			if (arquivoAudio.getImageAlbumByte() != null && arquivoAudio.getImageAlbumByte().length > 0) {
+
+				InputStream is = new ByteArrayInputStream(arquivoAudio.getImageAlbumByte());
+				Image imageAudio = new Image(is);
+
+				ivAudio.setImage(imageAudio);
+				
+				try {
+					is.close();
+				} catch (IOException e) {
+					log.error(e.getMessage());
+				}
+
+			} else {
+				ivAudio.setImage(null);
+			}
 		}
+	}
+	
+	public void ocultaLabelsInfoMediaPlayer(Scene scene) {
 		
+		final Label labelTitulo = (Label) scene.lookup("#labelTitulo");
+		labelTitulo.setVisible(Boolean.FALSE);
+
+		final Label labelAlbum = (Label) scene.lookup("#labelAlbum");
+		labelAlbum.setVisible(Boolean.FALSE);
+
+		final Label labelDuracao = (Label) scene.lookup("#labelDuracao");
+		labelDuracao.setVisible(Boolean.FALSE);
+	}
+	
+	public void exibeLabelsInfoMediaPlayer(Scene scene) {
+		
+		final Label labelTitulo = (Label) scene.lookup("#labelTitulo");
+		labelTitulo.setVisible(Boolean.TRUE);
+		
+		final Label labelAlbum = (Label) scene.lookup("#labelAlbum");
+		labelAlbum.setVisible(Boolean.TRUE);
+		
+		final Label labelDuracao = (Label) scene.lookup("#labelDuracao");
+		labelDuracao.setVisible(Boolean.TRUE);
 	}
 
 }
